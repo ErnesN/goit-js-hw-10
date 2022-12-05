0ab42b4a5a5d0fe918ee764d44d4839cd7e262fb
Ernes Nafieiev <demodekoz@gmail.com>
2022-12-05 14:38:22 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
-import { createMarkupCardCountry } from './js/createMarkupCardCountry';
 import { createListCountries } from './js/createListCountries';
+import { createMarkupCardCountry } from './js/createMarkupCardCountry';
 
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
@@ -15,29 +15,31 @@ searchBox.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
 function onInputChange(e) {
   const { value } = e.target;
+
   if (value.trim() === '') {
     clearMarkup();
   } else {
     fetchCountries(value.trim())
       .then(renderCardCountry)
-      .catch(_error =>
+      .catch(error =>
         Notiflix.Notify.failure('Oops, there is no country with that name')
       );
   }
 }
+
 function renderCardCountry(country) {
-  if (country.lenght > 10) {
+  if (country.length > 10) {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
   }
-  if (country.lenght >= 2 && country.lenght <= 10) {
+  if (country.length >= 2 && country.length <= 10) {
     const markup = country.map(createListCountries);
     clearMarkup();
     countryList.insertAdjacentHTML('beforeend', markup.join(''));
   }
 
-  if (country.lenght === 1) {
+  if (country.length === 1) {
     const markup = createMarkupCardCountry(country[0]);
     clearMarkup();
     countryInfo.innerHTML = markup;
